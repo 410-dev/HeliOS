87db12d07e21d38d30ffc2c9dd936d12653e814b
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-04-29 02:31:13 +0900

    Update 00-make-extensions.sh

--- src/gnome-extensions.debproj/_INFO/postinst.d/00-make-extensions.sh
+++ src/gnome-extensions.debproj/_INFO/postinst.d/00-make-extensions.sh
@@ -8,7 +8,7 @@ make
 
 echo "Building Blur My Shell extension..."
 cd /usr/share/gnome-shell/extensions
-unzip blur-my-shell@aunetx.shell-extension.zip -d blur-my-shell@aunetx
+unzip -o blur-my-shell@aunetx.shell-extension.zip -d blur-my-shell@aunetx
 glib-compile-schemas blur-my-shell@aunetx/schemas
 
 set +e
