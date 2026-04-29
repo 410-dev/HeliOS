dbca2376b77ee4c86d7b2e79f49fe453289a12da
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-04-29 20:58:58 +0900

    Added flatpak as a feature

---
+++ src/gnome-extensions.debproj/features/flatpak.feature.apprunxproj/main.py
@@ -0,0 +1,25 @@
+import sys
+from oscore.installer import Installer
+from AppContext import AppContext
+
+context = AppContext()
+context.ensure_single_process_globally()
+context.ensure_privileged()
+
+def main(args: list[str]) -> int:
+    installer = Installer("os.helios.feature.flatpak", True, "apt")
+    if "enable" in args:
+        installer.refresh()
+        installer.install(["flatpak"])
+        installer.install(["gnome-software-plugin-flatpak"])
+        installer.exec_shell(["flatpak", "remote-add", "--if-not-exists", "flathub", "https://flathub.org/repo/flathub.flatpakrepo"], [])
+        installer.commit_receipt()
+        return 0
+    elif "disable" in args:
+        installer.revert()
+        return 0
+    else:
+        return 1
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
