1e9ac98fbbfa24c1e4491af2696d5dcd74bc4474
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-05-08 09:26:43 +0900

    Added multi-monitors-bar extension

--- src/gnome-extensions.debproj/_INFO/postinst.d/00-install-extensions.sh
+++ src/gnome-extensions.debproj/_INFO/postinst.d/00-install-extensions.sh
@@ -11,4 +11,9 @@ cd /usr/share/gnome-shell/extensions
 unzip -o blur-my-shell@aunetx.shell-extension.zip -d blur-my-shell@aunetx
 glib-compile-schemas blur-my-shell@aunetx/schemas
 
+echo "Building Multi Monitor Bar extension..."
+cd /usr/share/gnome-shell/extensions
+unzip -o multi-monitors-bar@frederykabryan.zip -d multi-monitors-bar@frederykabryan
+glib-compile-schemas multi-monitors-bar@frederykabryan/schemas
+
 set +e
