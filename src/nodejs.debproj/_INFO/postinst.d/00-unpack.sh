2d365bc02ca280ca481016f44a07667b8917e8ba
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-05-15 14:19:33 +0900

    renamed misleading variable name

--- src/nodejs.debproj/_INFO/postinst.d/00-unpack.sh
+++ src/nodejs.debproj/_INFO/postinst.d/00-unpack.sh
@@ -4,15 +4,16 @@
 arch=$(uname -m)
 if [[ "$arch" == "x86_64" ]]; then
     echo "Detected architecture: x86_64"
-    POSTFIX="x64"
+    SUFFIX="x64"
 elif [[ "$arch" == "aarch64" ]]; then
     echo "Detected architecture: aarch64"
-    POSTFIX="arm64"
+    SUFFIX="arm64"
 else
     echo "Unsupported architecture: $arch"
     exit 1
 fi
-echo "Unpacking node-$POSTFIX.tar.gz..."
-mkdir -p {{install}}/node
-tar -xzf {{install}}/node-$POSTFIX.tar.gz -C {{install}}/node --strip-components=1
-echo "Unpacked node-$POSTFIX.tar.gz."
+echo "Unpacking node-$SUFFIX.tar.xz..."
+mkdir -p "{{install}}/26.1.0"
+tar -xJf "{{install}}/node-$SUFFIX.tar.xz" -C "{{install}}/26.1.0" --strip-components=1
+ln -sf "{{install}}/26.1.0" "{{install}}/latest"
+echo "Unpacked node-$SUFFIX.tar.xz."
