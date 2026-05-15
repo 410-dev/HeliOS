a586d74de6056e136188c42c70edd8d1484ff034
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-05-15 10:14:30 +0900

    Added gnome extensions: pinned-apps-in-appgrid, gnome-clipboard

--- src/gnome-extensions.debproj/_INFO/postinst.d/00-install-extensions.sh
+++ src/gnome-extensions.debproj/_INFO/postinst.d/00-install-extensions.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-# Gnome Clipboard History
 set -e
-echo "Building Gnome Clipboard History extension..."
-cd /usr/share/gnome-shell/extensions/clipboard-history@alexsaveau.dev
-make
+#echo "Building Gnome Clipboard History extension..."
+#cd /usr/share/gnome-shell/extensions/clipboard-history@alexsaveau.dev
+#make
 
-echo "Building Blur My Shell extension..."
+echo "Building Gnome Clipboard extension... (Credit: https://github.com/foss-desk/gnome-clipboard)"
+cd /usr/share/gnome-shell/extensions
+unzip -o gnome-clipboard-main.zip -d gnome-clipboard@foss-desk
+cd gnome-clipboard@foss-desk
+make install
+make enable
+
+
+echo "Building Blur My Shell extension... (Credit: https://github.com/aunetx/blur-my-shell)"
 cd /usr/share/gnome-shell/extensions
 unzip -o blur-my-shell@aunetx.shell-extension.zip -d blur-my-shell@aunetx
 glib-compile-schemas blur-my-shell@aunetx/schemas
 
-echo "Building Multi Monitor Bar extension..."
+echo "Building Multi Monitor Bar extension... (Credit: https://github.com/FrederykAbryan/multi-monitors-bar_fapv2)"
 cd /usr/share/gnome-shell/extensions
 unzip -o multi-monitors-bar@frederykabryan.zip -d multi-monitors-bar@frederykabryan
 glib-compile-schemas multi-monitors-bar@frederykabryan/schemas
+
+echo "Building Pinned Apps in AppGrid... (Credit: https://github.com/brunos3d/pinned-apps-in-appgrid)"
+cd /usr/share/gnome-shell/extensions/pinned-apps-in-appgrid
+make
 
 set +e
