67eed9b799901322bf7f25d02fa93738ada0d7eb
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-05-15 10:17:43 +0900

    As OS no longer has bootable snapshot feature, /boot mount and btrfs check is no longer necessary

--- src/core-skel.debproj/_INFO/preinst.d/00-compatibility-check.sh
+++ src/core-skel.debproj/_INFO/preinst.d/00-compatibility-check.sh
@@ -17,21 +17,21 @@ function keyread() {
 
 # Check if current system is installed in btrfs filesystem
 # If not, it is incompatible
-if [ "$(keyread BypassBTRFSCheck)" != "1" ]; then
-    if [ "$(findmnt -n -o FSTYPE /)" != "btrfs" ]; then
-        echo "Error: The root filesystem is not BTRFS. This system is incompatible."
-        exit 1
-    fi
-fi
-
-# Check if /boot is not mounted as separate partition
-# If so, then it is incompatible (Meaning: /boot MUST be a separate partition)
-if [ "$(keyread BypassBootPartitionCheck)" != "1" ]; then
-    if ! mountpoint -q /boot; then
-        echo "Error: /boot is not mounted as a separate partition. This system is incompatible."
-        exit 1
-    fi
-fi
+#if [ "$(keyread BypassBTRFSCheck)" != "1" ]; then
+#    if [ "$(findmnt -n -o FSTYPE /)" != "btrfs" ]; then
+#        echo "Error: The root filesystem is not BTRFS. This system is incompatible."
+#        exit 1
+#    fi
+#fi
+#
+## Check if /boot is not mounted as separate partition
+## If so, then it is incompatible (Meaning: /boot MUST be a separate partition)
+#if [ "$(keyread BypassBootPartitionCheck)" != "1" ]; then
+#    if ! mountpoint -q /boot; then
+#        echo "Error: /boot is not mounted as a separate partition. This system is incompatible."
+#        exit 1
+#    fi
+#fi
 
 
 # Host requirement: Ubuntu 26.04 LTS ONLY
